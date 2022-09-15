ce97988a8d73932a7742697487e8c8bd1bf07b1c
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 15:11:35 -0300

    Create modulo03-aula05-ex03.js

---
+++ modulo03-aula05-ex03.js
@@ -0,0 +1,10 @@
+///////------Variáveis------///////3) Crie uma variável valorComJuros que calcula o valor de um boleto acrescido de 10% de juros por atraso;
+vBoleto = 190
+
+///////------Calcula o valor do juros (10%)------///////
+juros = (vBoleto * 0.1)
+
+///////------Calcula o valor do boleto com 10% de juros------///////
+valorComJuros = (vBoleto * 1.1)
+
+console.log("Valor do boleto: R$" + vBoleto.toFixed(2) + "\nJuros: R$" + juros.toFixed(2) + "\nValor com juros: R$" + valorComJuros.toFixed(2))
