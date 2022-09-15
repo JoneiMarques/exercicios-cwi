346fd6fcd9c45ee0b52558f3f9f96b3b91158889
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 16:53:28 -0300

    Create modulo03-aula08-ex01.js

---
+++ modulo03-aula08-ex01.js
@@ -0,0 +1,7 @@
+///////------Arrays------///////
+capitais = ["Porto Alegre", "São Paulo", "Florianópolis", "Rio de Janeiro", "Salvador"]
+
+
+for ( i = 0; i < capitais.length; i++) {
+  console.log(capitais[i])
+}
