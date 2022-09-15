d9ed3bfeae19220d111f1ea3d892d8984fda39d3
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 14:32:35 -0300

    Create modulo03-aula05-ex01.js

---
+++ modulo03-aula05-ex01.js
@@ -0,0 +1,8 @@
+///////------Variáveis------///////
+nomeDoFulano = "Jonei"
+nomeDoBeltrano = "Jonei"
+
+///////------Verifica se os nomes São iguais------///////
+mesmoNome = (nomeDoFulano == nomeDoBeltrano)
+
+console.log(mesmoNome)
