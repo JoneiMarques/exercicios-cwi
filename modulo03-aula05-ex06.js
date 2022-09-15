7f5bce0736f6d3585ab840e7f33a02dee85c8a5c
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 16:13:17 -0300

    Create modulo03-aula05-ex06.js

---
+++ modulo03-aula05-ex06.js
@@ -0,0 +1,9 @@
+///////------Variáveis------///////
+saldoDaConta = 1000.00
+calca = 99.90
+salario = 2500.00
+moradorDeRua = 0.10
+
+saldoAtual = ((saldoDaConta - calca + salario - moradorDeRua) * 0.75)
+
+console.log("Saldo atual: R$ " + saldoAtual / 2)
