4fab03d091299ca38a4f00abfacbbed1cf41026f
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 15:45:45 -0300

    Create modulo03-aula05-ex05.js

---
+++ modulo03-aula05-ex05.js
@@ -0,0 +1,8 @@
+///////------Variáveis------///////
+receitaLiquida = 80000.00
+produtosVendidos = 15000.00
+
+lucroBruto = receitaLiquida - produtosVendidos
+margemBruta = (lucroBruto / receitaLiquida) * 100
+
+console.log("Lucro Bruto: R$ " + lucroBruto + "\nMargem Bruta: R$ " + margemBruta)
