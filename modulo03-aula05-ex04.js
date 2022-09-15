e2c5c600d7a53d796d1393630c2f41bd5db85971
Jonei Marques <113399566+JoneiMarques@users.noreply.github.com>
2022-09-15 15:23:53 -0300

    Create modulo03-aula05-ex04.js

---
+++ modulo03-aula05-ex04.js
@@ -0,0 +1,11 @@
+///////------Variáveis------///////
+item1 = 10
+item2 = 7.9
+item3 = 3
+item4 = 5.5
+item5 = 9
+
+///////------Calcula a média dos itens------///////
+mediaAritmetica = ((item1 + item2 + item3 + item4 + item5) /5)
+
+console.log("Média: " + mediaAritmetica)
